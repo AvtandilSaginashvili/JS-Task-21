7fff37d67c55ade6d82acd1d296b9cf2660d3163
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-06-09 11:45:22 +0400

    update

--- JS/script.js
+++ JS/script.js
@@ -7,17 +7,23 @@ function calc() {
     document.getElementById("result").innerHTML = Number (x) / Number ((y*y));
 
     if (bmi <= 18.4) {
-        document.getElementsById("noti").innerHTML = "You are underweight.";
+        document.getElementById("noti").innerHTML = "You are underweight.";
+        document.getElementById("noti").style.color = "#0000C8";
     }else if (bmi <= 24.9) {
         document.getElementById("noti").innerHTML = "You are healthy.";
+        document.getElementById("noti").style.color = "#00D200";
     }else if (bmi <= 29.9) {
         document.getElementById("noti").innerHTML = "You are over weight.";
+        document.getElementById("noti").style.color = "#B3D200";
     }else if (bmi <= 34.9) {
         document.getElementById("noti").innerHTML = "You are severely over weight.";
+        document.getElementById("noti").style.color = "#FFD200";
     }else if (bmi <= 39.9) {
         document.getElementById("noti").innerHTML = "You are obese.";
+        document.getElementById("noti").style.color = "#FF8800";
     }else {
         document.getElementById("noti").innerHTML = "You are severely obese.";
+        document.getElementById("noti").style.color = "#FF0000";
     }
 
     document.getElementById("myform").reset();
